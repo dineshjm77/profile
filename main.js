113f3089f4e971c6f46e0e4ca7dcf249d002b5d0
dineshjm77 <dineshkarthi11503@gmail.com>
2024-09-01 14:42:57 +0530

    add file

---
+++ main.js
@@ -0,0 +1,41 @@
+/*===================icon navbar==========*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+};
+
+/*=================scroll section active ===========*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    let top = window.scrollY;
+
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if (link.getAttribute('href').includes(id)) {
+                    link.classList.add('active');
+                }
+            });
+        }
+    });
+
+    /*==============sticky navbar================*/
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*================remove toggle icon and navbar==============*/
+    if (window.scrollY < 100) {
+        menuIcon.classList.remove('fa-xmark');
+        navbar.classList.remove('active');
+    }
+};
