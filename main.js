6b1b98d6ba689159801a977145ecb4cbfec4bdc2
dineshjm77 <dineshkarthi11503@gmail.com>
2024-09-05 15:20:23 +0530

    add file

--- main.js
+++ main.js
@@ -14,13 +14,13 @@ let navLinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
     let top = window.scrollY;
 
-    sections.forEach(sec => {
+    sections.forEach((sec) => {
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
         if (top >= offset && top < offset + height) {
-            navLinks.forEach(link => {
+            navLinks.forEach((link) => {
                 link.classList.remove('active');
                 if (link.getAttribute('href').includes(id)) {
                     link.classList.add('active');
